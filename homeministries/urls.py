c8d0574396d38354d6236a6246549321cff728c2
jrferguson <1q2w3e4r5t6y>
2023-08-23 21:37:19 +0100

    added ministry detail endpoint

--- homeministries/urls.py
+++ homeministries/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomeMinistriesView
+from .views import HomeMinistriesView, MinistryDetailView
 
 urlpatterns = [
     path("", HomeMinistriesView.as_view()),
+    path("ministry/<uuid:pk>", MinistryDetailView.as_view()),
 ]
